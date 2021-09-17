9cda7080c1b8743225848ce1e2be5582d8f612aa
Islam <i.kardanov@wisetech.pro>
2021-09-17 13:31:54 +0300

    Добавил документацию к коду программы.

--- generate_font_data_array.h
+++ generate_font_data_array.h
@@ -5,23 +5,64 @@
 #include <inttypes.h>
 #include <vector>
 
+/**
+ * @brief Класс для генерации массива, содержащие бинарные данные шрифта
+ */
 class FontDataArray {
 public:
-    typedef uint8_t Byte;
+    typedef uint8_t Byte; //!< объявление типа байт
 
+    /**
+     * @brief Конструктор инициализирует все внутренние переменные начальными значениями, которые ему передали
+     * @param arrayName имя массива, который будет сгенерирован
+     * @param height размер высоты данных шрифта в байтах
+     * @param width размер ширины данных шрифта в байтах
+     */
     FontDataArray(const std::string &arrayName, size_t height, size_t width);
+
+    /**
+     * @brief Конструктор уничтожает все внутренние ресурсы класса
+     */
     ~FontDataArray();
 
+    /**
+     * @brief оператор, для цтения юайтов из файла шрифта
+     * @param file ссылка на экземпляр файлового потока, для чтения байт данных из файла шрифта
+     * @return возвращает true в если чтение успешно, false иначе
+     */
     bool operator<< (std::ifstream &file);
 
+    /**
+     * @brief Генерирует хедер файл, содержащий объявление массива данных
+     * @param doc Документация к массиву
+     * @param includes Список всех подключаемых файлов внутри генерируемого хедера
+     * @param file ссылка на экземпляр файлового потока, для записи байт данных в сгенерированный файл
+     * @return возвращает true в если чтение успешно, false иначе
+     */
     bool generateHeaderFile(const std::string &doc, const std::vector<std::string> &includes, std::ofstream &file);
+
+    /**
+     * @brief Генерирует cpp файл, содержащий определение массива данных
+     * @param file ссылка на экземпляр файлового потока, для записи байт данных в сгенерированный файл
+     * @return возвращает true в если чтение успешно, false иначе
+     */
     bool generateSourceFile(std::ofstream &file);
 
 private:
+    /**
+     * @brief Считывает в массив байт из указанного файлового потока.
+     * @param file ссылка на экземпляр файлового потока, для чтения байт данных из файла шрифта
+     * @return
+     */
     Byte *readBytes(std::ifstream &file);
+
+    /**
+     * @brief Записывает в указанный файловый поток, массив байт
+     * @param file ссылка на экземпляр файлового потока, для записи байт данных в файл
+     */
     void writeBytes(std::ofstream &file);
 
-    std::string arrayName_;
-    Byte *data_;
-    size_t sizeArray_;
+    std::string arrayName_; //!< Названия генерируемого массива
+    Byte *data_; //!< Указатель на буфер данных шрифта
+    size_t sizeArray_; //!< размер буфера данных шрифта
 };
--- main.cpp
+++ main.cpp
@@ -1,31 +1,73 @@
 #include <iostream>
+#include <cstring>
 #include "generate_font_data_array.h"
 
-const std::string inFilePath = "/home/islam/CLionProjects/makeBitMaps/resources/defaultFontForFontManager.wfnt";
-const std::string outHeaderFilePath = "/home/islam/CLionProjects/makeBitMaps/resources/buffer.h";
-const std::string outSourceFilePath = "/home/islam/CLionProjects/makeBitMaps/resources/buffer.cpp";
 
-int main() {
+/**
+ * @brief Генериурет h и cpp файлы содержащие массив данных шрифта
+ * @param arrayName Название генерируемого буфера
+ * @param wfntFilePath Путь до файла со шрифтом
+ * @param height Высота текстуры со шрифтом
+ * @param width Ширина текстуры со шрифтом
+ * @return 0 если успешно, -1 иначе
+ */
+int GenerateFontDataArray(const std::string &arrayName, const std::string &wfntFilePath, size_t height, size_t width) {
+    FontDataArray fontDataArray(arrayName, height, width);
+    std::ifstream wfntFile(wfntFilePath, std::ios::in | std::ios::binary);
+    std::ofstream outHeaderFile(arrayName + ".h");
+    std::ofstream outSourceFile(arrayName + ".cpp");
 
-    FontDataArray fontArray("buffer", 128, 128);
-    std::ifstream inFile(inFilePath, std::ios::in | std::ios::binary);
-    std::ofstream outHeaderFile(outHeaderFilePath, std::ios::out);
-    std::ofstream outSourceFile(outSourceFilePath, std::ios::out);
+    if (!wfntFile.is_open()) {
+        std::cout << "File: " << wfntFilePath << "not found" << std::endl;
+        return -1;
+    }
 
-    fontArray << inFile;
+    fontDataArray << wfntFile;
 
-    fontArray.generateHeaderFile(
+    fontDataArray.generateHeaderFile(
         "Сгенерированные данные шрифта",
         {
         "#include \"wise_types.h\"",
         "#include \"containers/array.h\""
         }, outHeaderFile);
 
-    fontArray.generateSourceFile(outSourceFile);
+    fontDataArray.generateSourceFile(outSourceFile);
 
-    inFile.close();
+    wfntFile.close();
     outHeaderFile.close();
     outSourceFile.close();
-
     return 0;
+}
+
+/**
+ * @brief Выводит список флагом и параметров программы
+ * @return 0 если успешно, -1 иначе
+ */
+int PrintHelpInfo() {
+    std::cout << "Преобразует в массив данных из файла шрифта в header и cpp файлы содержащие бинарные данные"
+            << std::endl;
+    std::cout << "-g [arrayName] [fileName.wfnt] [height] [width]   генерирует данные шрифта" << std::endl;
+    std::cout << "-h                                                выводит справочник команд и флагов" << std::endl;
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int result = 0;
+
+    if (argc < 2 || argc > 6) {
+        std::cout << "Wrong arguments list" << std::endl;
+        result = -1;
+    }
+
+    const std::string flag = argv[1];
+    if (flag == "-g") {
+        result = GenerateFontDataArray(
+                argv[2], argv[3], atoi(argv[4]), atoi(argv[5]));
+    } else if (flag == "-h") {
+        result = PrintHelpInfo();
+    } else {
+        std::cout << "Wrong arguments list" << std::endl;
+        result = -1;
+    }
+    return result;
 }
